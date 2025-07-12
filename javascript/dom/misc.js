96072abd0266e3c73c4c9ce9b79eba72dc8b6eba
Arun Kumar <java.kumar.arun@gmail.com>
2025-07-12 14:21:06 +0530

    js

---
+++ javascript/dom/misc.js
@@ -0,0 +1,31 @@
+let student = {}
+student.name = "Ramesh"
+student.age = 32
+student.showDetails = function(){
+    console.log("I am a student of ABC college")
+}
+console.log(student)
+
+let fruits = []
+
+fruits.push("Mango");
+fruits.push("Apple");
+fruits.push("Guava");
+
+fruits.push("Grapes");
+fruits.push("Kiwi");
+
+fruits[3] = "Watermelon";
+
+console.log(fruits)
+
+
+/* Using javascript, write a program to insert fruits in an array and 
+    1. insert new item at last
+    2. sort them - ascending/descending    
+    3. insert at given position -> 3
+    4. remove from last
+    5. remove first
+    6. remove from given position
+*/
+
