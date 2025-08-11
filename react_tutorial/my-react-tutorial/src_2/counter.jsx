d49b89270b2bcf42edf2b699042a4426c8528d1e
Arun Kumar <java.kumar.arun@gmail.com>
2025-08-11 14:47:24 +0530

    component

--- react_tutorial/my-react-tutorial/src_2/counter.jsx
+++ react_tutorial/my-react-tutorial/src_2/counter.jsx
@@ -3,17 +3,23 @@ import { useState } from "react";
 export function Counter(){
     const title = "My Counter App";
     // let num = 45;
-    let [num, setNum] = useState(32)
+    let [num, setNum] = useState(0)
     const increment = function(){
         console.log("increasing num: "+num);
         setNum(num+1)
-        console.log("increased num: "+num);
+    }
+
+    const dec = function(){
+        num--;
+        console.log(num)
+        setNum(num)
     }
     return (
         <div> 
             <h4>{title}</h4>
             <h3>Num: {num}</h3>
             <button onClick={increment}>Increment</button>
+            <button onClick={dec}>Dec</button>
         </div>
     );
 }
